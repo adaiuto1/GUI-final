d4bd7cd25627ef3b3b37acd6562b461fbd943e4f
adaiuto1 <adaiuto@smu.edu>
2022-10-28 12:51:31 -0500

    include placeholder

--- frontend/src/Directory.js
+++ frontend/src/Directory.js
@@ -1,1 +1,16 @@
-export const accounts = [{}];
+//account type:
+// 1 = tenant
+// 2 = landlord
+// 3 = admin
+export const accounts = [
+    {
+        email: 'email@email',
+        password: 'password123',
+        accountType: 1,
+        firstName: 'bob',
+        lastName: 'jones',
+        age: 35,
+        bio: "Lorem Ipsum",
+        tags: ['placeholder', 'enthusiast', 'legos']
+    }
+];
