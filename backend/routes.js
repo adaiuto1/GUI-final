36bde6f72641672b7a1270cef261bf18aa0b30dc
chadbraden <cbraden@smu.edu>
2022-11-10 15:35:27 -0600

    changes

--- backend/routes.js
+++ backend/routes.js
@@ -11,7 +11,7 @@ module.exports = function routes(app, logger) {
     // obtain a connection from our pool of connections
     pool.getConnection(function (err, connection){
       if (err){
-        console.log(connection);
+        console.log(err);
         // if there is an issue obtaining a connection, release the connection instance and log the error
         logger.error('Problem obtaining MySQL connection', err)
         res.status(400).send('Problem obtaining MySQL connection'); 
@@ -97,7 +97,7 @@ module.exports = function routes(app, logger) {
   });
 
   // POST user
-  app.post('/user', (req, res) => {
+  app.post('/Users', (req, res) => {
     // obtain a connection from our pool of connections
     pool.getConnection(function (err, connection){
       if (err){
@@ -107,14 +107,14 @@ module.exports = function routes(app, logger) {
         res.status(400).send('Problem obtaining MySQL connection'); 
       } else {
         // if there is no issue obtaining a connection, execute query
-        connection.query('SELECT * FROM User WHERE username = ?', req.body.username, function (err, rows, fields) {
+        connection.query('SELECT * FROM Users WHERE username = ?', req.body.username, function (err, rows, fields) {
           if (err) { 
             // if there is an error with the query, release the connection instance and log the error
             connection.release()
             res.status(400).send('Username already exists, please enter a new username'); 
           } else {
             // if there is no error with the query, execute the next query and do not release the connection yet
-            connection.query('INSERT INTO User(name, birthday, username, password) VALUES(?,?,?,?)', [req.body.name, req.body.birthday, req.body.username, req.body.password], function (err, rows, fields) {
+            connection.query('INSERT INTO Users(name, birthday, username, password) VALUES(?,?,?,?)', [req.body.name, req.body.birthday, req.body.username, req.body.password], function (err, rows, fields) {
               if (err) { 
                 // if there is an error with the query, release the connection instance and log the error
                 connection.release()
@@ -133,7 +133,7 @@ module.exports = function routes(app, logger) {
   });
 
   // GET user
-  app.get('/user', (req, res) => {
+  app.get('/Users', (req, res) => {
     // obtain a connection from our pool of connections
     pool.getConnection(function (err, connection){
       if(err){
