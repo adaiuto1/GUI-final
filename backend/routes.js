2e0110b2dac463ccf7ab2b29017c603528533222
chadbraden <cbraden@smu.edu>
2022-11-10 17:31:57 -0600

    quick changes

--- backend/routes.js
+++ backend/routes.js
@@ -114,7 +114,7 @@ module.exports = function routes(app, logger) {
             res.status(400).send('Username already exists, please enter a new username'); 
           } else {
             // if there is no error with the query, execute the next query and do not release the connection yet
-            connection.query('INSERT INTO Users(name, birthday, username, password) VALUES(?,?,?,?)', [req.body.name, req.body.birthday, req.body.username, req.body.password], function (err, rows, fields) {
+            connection.query('INSERT INTO Users(name, username, password) VALUES(?,?,?,?)', [req.body.name, req.body.username, req.body.password], function (err, rows, fields) {
               if (err) { 
                 // if there is an error with the query, release the connection instance and log the error
                 connection.release()
