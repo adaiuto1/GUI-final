286c3645d49bea05289eb5d73cace2e616376ad6
adaiuto1 <adaiuto@smu.edu>
2022-11-10 17:38:41 -0600

    update placeholder data

---
+++ frontend/src/data/AccountList.js
@@ -0,0 +1,38 @@
+export const AccountList = [
+    {
+        email: "bobjones@email.com",
+        password: "password123",
+        accountType: 1,
+        accountId: 1
+    },
+    {
+        email: "joemarsh@t1.com",
+        password: "t00b4s3d",
+        accountType: 1,
+        accountId: 2
+    },
+    {
+        email: "humzhlol@apt.ny",
+        password: "pdfhunter",
+        accountType: 1,
+        accountId: 3
+    },
+    {
+        email: "cboucher@email.com",
+        password: "YNAAAYT",
+        accountType: 1,
+        accountId: 4
+    },
+    {
+        email: "charlotteh@hotmail.co.uk",
+        password: "r34dyt0f1y",
+        accountType: 2,
+        accountId: 5
+    },
+    {
+        email: "tomcat_robson@email.com",
+        password: "fearthemic42",
+        accountType: 2,
+        accountId: 6
+    }
+]
