997778fd54e77046ddb5811d386b5b7af211e523
Mikecamdo <mcdoherty@smu.edu>
2022-11-29 12:18:59 -0600

    Copied backend files from profiles_testing
    Copied backend files from profiles_testing

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-const util = require('util');
 
 // mysql connection
 var pool = mysql.createPool({ //what is this
--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,17 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
-
 // const mysqlConnect = require('./db');
-//this is all trying to work with knex based on the base given from lab
 const routes = require('./routes');
-const { createModelsMiddleware  } = require('./middleware/model-middleware' );
 
 // set up some configs for express.
 const config = {
-  host: process.env.MYSQL_CLOUD_HOST,
-  port: process.env.MYSQL_PORT,
-  name: 'roommate-finder-express-app',
-  
+  name: 'sample-express-app',
+  port: 8000,
+  host: '0.0.0.0',
 };
 
-// create the express.js object,  I guess this is the app? 
+// create the express.js object
 const app = express();
 
 // create a logger object.  Using logger is preferable to simply writing to the console.
@@ -26,7 +22,6 @@ const logger = log({ console: true, file: false, label: config.name });
 
 // specify middleware to use
 app.use(bodyParser.json());
-app.use(createModelsMiddleware);
 app.use(cors({
   origin: '*'
 }));
@@ -42,5 +37,3 @@ app.listen(config.port, config.host, (e) => {
   }
   logger.info(`${config.name} running on ${config.host}:${config.port}`);
 });
-
-
