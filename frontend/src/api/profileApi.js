bbac6a6724a76dbbba01a913cb1409db6ecb654f
adaiuto1 <adaiuto@smu.edu>
2022-11-29 18:23:12 -0600

    mergred

--- frontend/src/api/profileApi.js
+++ frontend/src/api/profileApi.js
@@ -8,13 +8,3 @@ export const createProfile = (profile) =>{
 export const getProfileById = (id) =>{
    return axios.get(`${apiEndpoint}/profiles/${id}`).then(x=>x.data)
 }
-import axios from 'axios';
-import { useContext } from 'react';
-const apiEndpoint = '//localhost:8000';
-
-export const createProfile = (profile) =>{
-    axios.post(`${apiEndpoint}/profiles/`, profile);
-}
-export const getProfileById = (id) =>{
-   return axios.get(`${apiEndpoint}/profiles/${id}`).then(x=>x.data)
-}
