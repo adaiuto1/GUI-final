2012162dd3c1f76fd692b52cfe1a57ee3773c73b
Michael Amberg <89410893+SaltyPeachcake@users.noreply.github.com>
2022-11-29 19:43:46 -0600

    Updated /properties to include more columns in table

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -7,30 +7,98 @@ CREATE DATABASE IF NOT EXISTS db;
 USE db;
 
 -- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+create table applications
+(
+    tenent      int        null,
+    landlord    int        not null,
+    property_id int        not null,
+    approved    tinyint(1) null,
+    response    int        null,
+    primary key (landlord, property_id),
+    constraint applications_Users_null_fk
+        foreign key (landlord) references Users (user_id),
+    constraint applications_Users_null_fk2
+        foreign key (tenent) references Users (user_id),
+    constraint applications_property_table_null_fk
+        foreign key (property_id) references property_table (propertyId)
 );
 
-CREATE TABLE users_table(
-    user_id INT IDENTITY(1,1) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE
-)
+create table comments
+(
+    property_id int  not null
+        primary key,
+    user_id     int  not null,
+    comment     text not null,
+    constraint comments_Users_null_fk
+        foreign key (user_id) references Users (user_id),
+    constraint comments_property_table_null_fk
+        foreign key (property_id) references property_table (propertyId)
+);
 
-CREATE TABLE profiles(
-    user_id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    age INT,
-    gender VARCHAR(255),
-    interests TEXT,
-    image_url VARCHAR(255),
-    looking_for TEXT,
-    user_status TEXT,
-    rating FLOAT
-)
+create table landlord_rating
+(
+    num_rating  int   default 0 null,
+    rating      float default 0 null,
+    landlord_id int             not null
+        primary key,
+    constraint landlord_rating_Users_null_fk
+        foreign key (landlord_id) references Users (user_id)
+);
 
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+create table profiles
+(
+    user_id     int                  not null
+        primary key,
+    firstname   varchar(255)         not null,
+    lastname    varchar(255)         not null,
+    smoker      tinyint(1) default 0 null,
+    petFriendly tinyint(1) default 0 null,
+    bio         text                 null,
+    tag1        tinyint(1) default 0 null,
+    tag2        tinyint(1) default 0 null,
+    tag3        tinyint(1) default 0 null,
+    tag4        tinyint(1) default 0 not null,
+    tag5        tinyint(1) default 0 null,
+    tag6        tinyint(1) default 0 null,
+    constraint user_id
+        foreign key (user_id) references Users (user_id)
+);
+
+create table property_table
+(
+    address       varchar(255)         not null,
+    city          varchar(255)         null,
+    zipcode       int                  null,
+    propertyId    int auto_increment
+        primary key,
+    monthlyRent   int                  null,
+    owner         int                  null,
+    ratingSum     double               null,
+    numRatings    int                  null,
+    capacity      int                  null,
+    sqft          int                  null,
+    allowsPets    tinyint(1) default 0 null,
+    allowsSmoking tinyint(1) default 0 null,
+    img           varchar(255)         null,
+    tag1          tinyint(1) default 0 null,
+    tag2          tinyint(1) default 0 null,
+    tag3          tinyint(1) default 0 null,
+    tag4          tinyint(1) default 0 null,
+    tag5          tinyint(1) default 0 null,
+    tag6          tinyint(1) default 0 null,
+    tag7          tinyint(1) default 0 null,
+    constraint owner
+        foreign key (owner) references Users (user_id)
+);
+
+create table Users
+(
+    user_id      int auto_increment
+        primary key,
+    password     varchar(255)  not null,
+    username     varchar(35)   not null,
+    account_type int default 1 null,
+    constraint Users_username_uindex
+        unique (username)
+);
+
--- backend/routes.js
+++ backend/routes.js
@@ -20,22 +20,22 @@ module.exports = function routes(app, logger) {
   });
   
 
-  // insert a newly created user into the database 
-  // POST /createprofile
-  app.post('/profiles', async (req, res) => {
+// insert a newly created user into the database 
+   // POST /createprofile
+   app.post('/profiles', async (req, res) => {
     console.log('\n' + req.body.firstname);
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // const id = req.params.id; // And pull the ID from the req params
-        const payload = req.body; // This payload should be an object containing update profile data
-        // if there is no issue obtaining a connection, execute query and release connection
-        var query = 'INSERT INTO profiles (firstname, lastname, user_id, bio, smoker, petFriendly, tag1, tag2, tag3, tag4, tag5, tag6 ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
-        //none of this is reffered to as the payload now, update it
-        connection.query(query,[payload.firstname, payload.lastname, payload.user_id, payload.bio, payload.smoker, payload.petFriendly,
+   pool.getConnection(function (err, connection){
+    if(err){
+      // if there is an issue obtaining a connection, release the connection instance and log the error
+      logger.error('Problem obtaining MySQL connection',err)
+      res.status(400).send('Problem obtaining MySQL connection'); 
+    } else {
+      // const id = req.params.id; // And pull the ID from the req params
+      const payload = req.body; // This payload should be an object containing update profile data
+      // if there is no issue obtaining a connection, execute query and release connection
+      var query = 'INSERT INTO profiles (firstname, lastname, user_id, bio, smoker, petFriendly, tag1, tag2, tag3, tag4, tag5, tag6 ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
+      //none of this is reffered to as the payload now, update it
+      connection.query(query,[payload.firstname, payload.lastname, payload.user_id, payload.bio, payload.smoker, payload.petFriendly,
         payload.tag1, payload.tag2, payload.tag3, payload.tag4, payload.tag5, payload.tag6], function (err, rows, fields) {
         connection.release();
         if (err) {
@@ -138,7 +138,7 @@ app.get('/profiles/:id', async (req, res) => {
     } else {
       const id = req.params.id;
       // if there is no issue obtaining a connection, execute query and release connection
-      connection.query('SELECT * FROM profiles WHERE id = ?', [id], function (err, rows, fields) {
+      connection.query('SELECT * FROM profiles WHERE user_id = ?', [id], function (err, rows, fields) {
         connection.release();
         if (err) {
           logger.error("Error while fetching profile: \n", err);
@@ -298,7 +298,8 @@ app.post('/reset', (req, res) => {
         res.status(400).send('Problem obtaining MySQL connection'); 
       } else {
         // if there is no issue obtaining a connection, execute query and release connection
-        connection.query('SELECT * FROM Users', function (err, rows, fields) {
+        const query = req.query.username ? `SELECT * FROM Users WHERE username = '${req.query.username}'` : 'SELECT * FROM Users';
+        connection.query(query, function (err, rows, fields) {
           connection.release();
           if (err) {
             logger.error("Error while fetching users: \n", err);
@@ -495,16 +496,15 @@ app.post('/reset', (req, res) => {
     }
     });
     })
-  
-  // insert a newly created user into the database 
-  // POST /createprofile
+
+    // POST /createprofile
   app.post('/application', async (req, res) => {
     console.log('\n' + req.body.firstname);
     pool.getConnection(function (err, connection){
       if(err){
         // if there is an issue obtaining a connection, release the connection instance and log the error
         logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
+        res.status(400).send('Problem obtaining MySQL connection');
       } else {
         // const id = req.params.id; // And pull the ID from the req params
         const payload = req.body; // This payload should be an object containing update profile data
@@ -530,12 +530,12 @@ app.post('/reset', (req, res) => {
   });
 
   // edit application
-  app.put('/application', async (req, res) => { 
+  app.put('/application', async (req, res) => {
     pool.getConnection(function (err, connection){
       if(err){
         // if there is an issue obtaining a connection, release the connection instance and log the error
         logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
+        res.status(400).send('Problem obtaining MySQL connection');
       } else {
         const payload = req.body; // This payload should be an object containing update profile data
         const id = req.params.id; // And pull the ID from the req params
@@ -559,4 +559,150 @@ app.post('/reset', (req, res) => {
       }
     });
   });
+
+    //comments functions
+    app.post('/comment', async (req, res) => {
+    pool.getConnection(function (err, connection){
+     if(err){
+       // if there is an issue obtaining a connection, release the connection instance and log the error
+       logger.error('Problem obtaining MySQL connection',err)
+       res.status(400).send('Problem obtaining MySQL connection');
+     } else {
+       const body = req.body; // This payload should be an object containing update profile data
+       // if there is no issue obtaining a connection, execute query and release connection
+       var query = 'INSERT INTO comments(property_id, user_id, comment)'
+       //none of this is reffered to as the payload now, update it
+       connection.query(query,[body.property_id, body.user_id, body.comment], function (err, rows, fields) {
+         connection.release();
+         if (err) {
+           logger.error("Error while posting the comment: \n", err);
+           res.status(400).json({
+             "data": [],
+             "error": "Error creating comment"
+           })
+         } else {
+           res.status(200).json({
+             "data": rows
+           });
+         }
+       });
+     }
+     });
+   });
+
+   app.get('/comment/:id', async (req, res) => {
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        const id = req.params.id;
+        connection.query('SELECT * FROM comments WHERE property_id = ?', [id], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching comments: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining comments"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+
+   app.delete('/comment/:id/:prop_id', async (req, res) => {
+    pool.getConnection(function (err, connection){
+     if(err){
+       // if there is an issue obtaining a connection, release the connection instance and log the error
+       logger.error('Problem obtaining MySQL connection',err)
+       res.status(400).send('Problem obtaining MySQL connection');
+     } else {
+       const body = req.body; // This payload should be an object containing update profile data
+       // if there is no issue obtaining a connection, execute query and release connection
+
+       //none of this is reffered to as the payload now, update it
+       connection.query('DELETE FROM comments WHERE property_id = ? AND user_id = ?', [body.property_id, body.user_id], function (err, rows, fields) {
+         connection.release();
+         if (err) {
+           logger.error("Error while deleting the comment: \n", err);
+           res.status(400).json({
+             "data": [],
+             "error": "Error deleting comment"
+           })
+         } else {
+           res.status(200).json({
+             "data": rows
+           });
+         }
+       });
+     }
+     });
+   });
+
+
+   //landlord rating calls
+
+   app.post('/rating', async (req, res) => {
+    pool.getConnection(function (err, connection){
+     if(err){
+       // if there is an issue obtaining a connection, release the connection instance and log the error
+       logger.error('Problem obtaining MySQL connection',err)
+       res.status(400).send('Problem obtaining MySQL connection');
+     } else {
+       const body = req.body; // This payload should be an object containing update profile data
+       // if there is no issue obtaining a connection, execute query and release connection
+       var query = 'INSERT INTO landlord_rating(landlord_id, rating)'
+       //none of this is reffered to as the payload now, update it
+       connection.query(query,[body.landlord_id, body.rating], function (err, rows, fields) {
+         connection.release();
+         if (err) {
+           logger.error("Error while posting the rating: \n", err);
+           res.status(400).json({
+             "data": [],
+             "error": "Error submitting rating"
+           })
+         } else {
+           res.status(200).json({
+             "data": rows
+           });
+         }
+       });
+     }
+     });
+   });
+
+    app.get('/rating/:id', async (req, res) => {
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        const id = req.params.id;
+        connection.query('SELECT * FROM landlord_rating WHERE landlord_id = ?', [id], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching ratings: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining ratings"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+
 }
