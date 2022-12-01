0f167167fc9e600d6995fd87cff92795313eaefb
adaiuto1 <adaiuto@smu.edu>
2022-12-01 13:27:42 -0600

    needs to update api location

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useContext } from 'react';
-const apiEndpoint = 'http://ec2-54-172-10-241.compute-1.amazonaws.com:8000';
+const apiEndpoint = '//localhost:8000';
 
 
 export const getUsers = () => {
