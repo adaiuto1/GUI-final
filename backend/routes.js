d9f72f0bd5853f3907759ef3b57af33722ba66a9
Chad Braden <78322706+chadbraden@users.noreply.github.com>
2022-11-30 19:28:37 -0600

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -629,7 +629,7 @@ app.post('/reset', (req, res) => {
       } else {
         // if there is no issue obtaining a connection, execute query and release connection
         const id = req.params.id;
-        connection.query('SELECT * FROM comments WHERE comment_id = ?', req.params.comment_id, function (err, rows, fields) {
+        connection.query('SELECT * FROM comments WHERE comment_id = ?', req.params.id, function (err, rows, fields) {
           connection.release();
           if (err) {
             logger.error("Error while fetching comments: \n", err);
