ecd9b519184c881b787f7ec5a3c11da5f7a68ae7
j-kynerd <jkynerd@smu.edu>
2022-12-01 00:53:44 -0600

    replaced api endpoint

--- frontend/src/api/getterApi.js
+++ frontend/src/api/getterApi.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-import React from "react";
-import { useState } from "react";
 import { AccountList } from "../data/AccountList";
-const baseEndpoint = "https://localhost:8000"
 
 export var currentUser = undefined;
 export var filters = [];
