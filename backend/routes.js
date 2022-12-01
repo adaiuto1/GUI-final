8a14548ab91dd0447d788abec30d538ca6bd6ab5
Chad Braden <78322706+chadbraden@users.noreply.github.com>
2022-11-30 19:18:34 -0600

    update edit applications

--- backend/routes.js
+++ backend/routes.js
@@ -603,9 +603,9 @@ app.post('/reset', (req, res) => {
      } else {
        const body = req.body; // This payload should be an object containing update profile data
        // if there is no issue obtaining a connection, execute query and release connection
-       var query = 'INSERT INTO comments(property_id, user_id, comment)'
+       var query = 'INSERT INTO comments(property_id, user_id, comment, comment_id)'
        //none of this is reffered to as the payload now, update it
-       connection.query(query,[body.property_id, body.user_id, body.comment], function (err, rows, fields) {
+       connection.query(query,[body.property_id, body.user_id, body.comment, body.comment_id], function (err, rows, fields) {
          connection.release();
          if (err) {
            logger.error("Error while posting the comment: \n", err);
@@ -622,6 +622,34 @@ app.post('/reset', (req, res) => {
      }
      });
    });
+
+     // POST user
+  app.post('/application', (req, res) => {
+    console.log(req.body);
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if (err){
+        console.log(connection);
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection', err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        // if there is no error with the query, execute the next query and do not release the connection yet
+        connection.query('INSERT INTO applications(tenant, landlord, property_id, response, application_id) VALUES(?,?,?,?,?)', [req.body.tenant, req.body.landlord, req.body.property_id, req.body.response, req.body.application_id], function (err, rows, fields) {
+          if (err) { 
+            // if there is an error with the query, release the connection instance and log the error
+            connection.release()
+            logger.error("Problem creating application: \n", err);
+            res.status(400).send('Problem creating application'); 
+          } else { 
+            // if there is no error with the query, release the connection instance
+            connection.release()
+            res.status(200).send('created new application'); 
+          }
+        });
+      }
+    });
+  });
 
    app.get('/comment/:id', async (req, res) => {
     pool.getConnection(function (err, connection){
